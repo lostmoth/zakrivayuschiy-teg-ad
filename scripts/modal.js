815e22dee3ddb6b28748a38d18dfc7b90ae826b1
lostmoth <lostmoth@yandex.ru>
2026-03-26 21:34:21 +0300

    тестовый коммит 2

---
+++ scripts/modal.js
@@ -0,0 +1,27 @@
+const saveBtn = document.querySelector('.save-button');
+const dialog = document.getElementById('saveDialog');
+const okBtn = document.getElementById('saveOk');
+
+if (saveBtn && dialog) {
+  saveBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (typeof dialog.showModal === 'function') {
+      dialog.showModal();
+    } else {
+      dialog.setAttribute('open', '');
+    }
+  });
+}
+
+if (okBtn && dialog) {
+  okBtn.addEventListener('click', () => {
+    dialog.close();
+  });
+}
+
+// close on outside click
+if (dialog) {
+  dialog.addEventListener('click', (e) => {
+    if (e.target === dialog) dialog.close();
+  });
+}
